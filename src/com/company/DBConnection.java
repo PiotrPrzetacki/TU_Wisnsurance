aaef1f82571cc85870d8c60cf5823582ce41b7a1
Piotr Przetacki <pprzetacki@tm1.edu.pl>
2022-04-28 08:57:30 +0200

    Add database connection

---
+++ src/com/company/DBConnection.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DBConnection {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void createTables(){
+        if(statement!=null) {
+            try {
+                Scanner scanner = new Scanner(new File("src/ddl.txt"));
+                String sql = "";
+                while (scanner.hasNext()) {
+                    while (!sql.contains(";")) {
+                        sql += scanner.nextLine();
+                    }
+                    statement.execute(sql);
+                    sql = "";
+                }
+                System.out.println(sql);
+            } catch (FileNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void connect(String url){
+        try {
+            connection = DriverManager.getConnection(url);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void disconnect(){
+        if(connection!=null){
+            try {
+                connection.close();
+                statement.close();
+                statement = null;
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DBConnection.connect("jdbc:sqlite:db.sqlite");
+        DBConnection.createTables();
+        DBConnection.disconnect();
     }
 }
